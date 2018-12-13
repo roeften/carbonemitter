4671b93a3c797998cbaa465a8285ecc34ab670a5
Panos Gkikakis <3620308+roeften@users.noreply.github.com>
2018-12-13 17:17:31 +0200

    Minor fixes

--- __init__.py
+++ __init__.py
@@ -2,10 +2,10 @@ import sys
 
 PY35 = sys.version_info[0] > 2 and sys.version_info[1] > 4
 
-get_carbon():
+def get_carbon(**kwargs):
     if PY35:
         from async_graphite import CarbonEmitter
     else:
-        from graphite import CarbonEmitter(**kwargs)
+        from graphite import CarbonEmitter
     
     return CarbonEmitter(**kwargs)
